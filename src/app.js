1a053cbd0ddf63cc556aed6a4e69ab3433faf72b
MilosSimic994 <60389147+MilosSimic994@users.noreply.github.com>
2021-06-15 11:04:11 +0200

    tablet, mob view

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ const calcluate = () => {
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      console.log(data.rates);
+
       const rate = data.rates[currency_two];
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
